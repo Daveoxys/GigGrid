a8902f5e26c4230fad4525ba4e56bb476e1a9cda
Daveoxys <davidtaylor970@googlemail.com>
2025-08-03 16:35:17 +0100

    Some basic changes, renaming of fragments etc

--- app/src/main/java/com/example/GigGrid/ui/slideshow/MyEventsFragment.kt
+++ app/src/main/java/com/example/GigGrid/ui/slideshow/MyEventsFragment.kt
@@ -1,4 +1,4 @@
-package com.example.GigGrid.ui.slideshow
+package com.example.GigGrid.ui.eventDiary
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.GigGrid.databinding.FragmentSlideshowBinding
+import com.example.GigGrid.databinding.FragmentEventDiaryBinding
 
-class SlideshowFragment : Fragment() {
+class EventDiaryFragment : Fragment() {
 
-    private var _binding: FragmentSlideshowBinding? = null
+    private var _binding: FragmentEventDiaryBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class SlideshowFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val slideshowViewModel =
-            ViewModelProvider(this).get(SlideshowViewModel::class.java)
+        val galleryViewModel =
+            ViewModelProvider(this)[EventDiaryViewModel::class.java]
 
-        _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
+        _binding = FragmentEventDiaryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textSlideshow
-        slideshowViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textGallery
+        galleryViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
